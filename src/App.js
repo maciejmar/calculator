8e8d1579a3166183c324e64dd9f7c920f8b6c147
macmaro <mateminfo@gmail.com>
2022-01-12 00:56:05 +0100

    styling of divs

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import logo from './logo.svg';
+import "./styles.css"
 import './App.css';
 
 function App() {
@@ -7,8 +8,25 @@ function App() {
      <div className="output">
        <div className = "previous-operand"></div>
        <div className = "previous-operand"> </div>
-
      </div>
+     <button className = "span-two">AC</button>
+     <button >DEL</button>
+     <button >÷</button>
+     <button >1</button>
+     <button >2</button>
+     <button >3</button>
+     <button >*</button>
+     <button >4</button>
+     <button >5</button>
+     <button >6</button>
+     <button >+</button>
+     <button >7</button>
+     <button >8</button>
+     <button >9</button>
+     <button >-</button>
+     <button >.</button>
+     <button >0</button>
+     <button className = "span-two" >=</button>
 
    </div>
   );
